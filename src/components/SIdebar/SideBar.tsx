1c2a15eab9c79e11eba6128ddfe7ab61d368d1af
Bogdan-Raliuk <bogdanraliuk@gmail.com>
2023-10-12 15:54:14 +0200

    add side bar placeholder

--- src/components/SIdebar/SideBar.tsx
+++ src/components/SIdebar/SideBar.tsx
@@ -3,7 +3,23 @@ import styles from "./SideBar.module.scss";
 interface SideBarProps {}
 
 const SideBar: React.FC<SideBarProps> = () => {
-	return <aside className={styles.sideBar}>side bar</aside>;
+	return (
+		<aside className={styles.sideBar}>
+			<select>
+				<option hidden>country</option>
+			</select>
+			<p>current temperature</p>
+			<p>wind speed</p>
+			<hr />
+			<h3>the next days forecast</h3>
+
+			<button>5 days</button>
+			<button>14 days</button>
+			<button>30 days</button>
+
+			<p>Day of the week</p>
+		</aside>
+	);
 };
 
 export default SideBar;
